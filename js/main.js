bef9eba5c6850769bb426115077f4f066aa69507
Jacob M <moe.jacobw@gmail.com>
2019-11-05 11:03:26 -0600

    added hamburger menu for mobile, fixed projects for larger screens

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    let y = document.getElementById('myLinks');
+    if (y.style.display === "block") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "block";
+    }
+}
+
+function displayLinks() {
+    
+}
